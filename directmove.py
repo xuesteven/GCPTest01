356843f7bdfab1a33fc9895272159807d8ae61f5
xuesteven <32335689+xuesteven@users.noreply.github.com>
2018-06-10 22:48:23 -0400

    Add files via upload

---
+++ directmove.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import apache_beam as beam
+import re
+
+
+PROJECT='stevenxue20181006'
+BUCKET='stevenxue20181006'
+
+def run():
+   argv = [
+      '--project={0}'.format(PROJECT),
+      '--job_name=examplejob2',
+      '--save_main_session',
+      '--staging_location=gs://{0}/staging/'.format(BUCKET),
+      '--temp_location=gs://{0}/staging/'.format(BUCKET),
+      '--runner=DataflowRunner'
+   ]
+
+   p = beam.Pipeline(argv=argv)
+   input = 'gs://{0}/test01.txt'.format(BUCKET)
+   output_prefix = 'gs://{0}/test01'.format(BUCKET)
+
+   # find all lines that contain the searchTerm
+   (p
+      | 'GetFile' >> beam.io.ReadFromText(input)
+      | 'write' >> beam.io.WriteToText(output_prefix)
+   )
+
+   p.run()
+
+if __name__ == '__main__':
+   run()
